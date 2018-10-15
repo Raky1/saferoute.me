62ea1a1a7cb0747699d2cd148c20a3ee179e66be
Maick Henrique <contatomaickh@gmail.com>
2018-10-15 19:40:15 -0300

    acrescimo de fontes

--- application/views/map.php
+++ application/views/map.php
@@ -19,6 +19,9 @@
     <!--estilo css-->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/estilo.css'); ?>">
 
+    <!--Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anton|Ubuntu" rel="stylesheet">
+
     <!-- leaflet js -->
     <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
     integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="

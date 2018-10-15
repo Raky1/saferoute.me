eb11b9c253db1902f7871b632118ed896f5198e1
Maick Henrique <contatomaickh@gmail.com>
2018-10-15 19:07:39 -0300

    Update application/views/includes/menu.php
    Menu foi alterado

--- application/views/includes/menu.php
+++ application/views/includes/menu.php
@@ -1,2 +1,53 @@
-<a href="<?php echo site_url('map'); ?>">Mapa</a><br>
-<a href="<?php echo site_url('occurrence'); ?>">Fui Roubado</a><br>
+<div class="logo">
+    <img src="assets/images/logopng.png">
+</div>
+<div class="rodape container fixed-top">
+  <ul class="nav">
+  	<li class="nav-item">
+      <p class="email">Email:contatosaferoute.me</p>
+    </li>
+  </ul>
+ <!--<div class="login-registro">
+  <ul class="nav justify-content-end">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Registre-se</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Login</a>
+    </li>
+  </ul>
+  </div>-->
+</div>
+
+<div class="hr fixed-top" >
+	<hr>
+</div>
+
+<nav class="nav-header navbar navbar-expand-sm  fixed-top justify-content-end">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="index.php"><u>H</u>ome</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+       Saferoute
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Sobre</a>
+        <a class="dropdown-item" href="#">Download Android</a>
+        <a class="dropdown-item" href="#">Anuncie Aqui</a>
+        <a class="dropdown-item" href="#">Blog</a>
+      </div>
+    </li>
+    <li class="nav-item">
+    	<a class="nav-link" href="">Anuncie no Saferoute</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo site_url('map'); ?>">Mapa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo site_url('occurrence'); ?>">Fui Roubado</a>
+    </li>
+  </ul>
+</nav>
+

ef54c573ea789243ac0e3b8a439d5fc409b4ffc9
Maick Henrique <contatomaickh@gmail.com>
2018-10-16 01:17:10 -0300

    fui_roubado

--- application/views/fui_roubado.php
+++ application/views/fui_roubado.php
@@ -13,6 +13,12 @@
     <!-- map search box css -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/map-search-box.css'); ?>">
 
+     <!--estilo css-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/estilo.css'); ?>">
+
+    <!--Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Titillium+Web|Ubuntu" rel="stylesheet">
+
     <!-- leaflet js -->
     <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
     integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
@@ -30,32 +36,28 @@
     <!-- message div -->
     <?php $this->load->view('includes/msg'); ?>
 
-    <div id="map" style="height : 350px;"></div>
+    <div id="map" style="height :500px; margin-top: 45px;"></div>
     
+<div class="form-roubo">
     <form autocomplet=off action="occurrence/insert" method="POST">
 
-		Endereço: 
-        <input id=input_address type=text name=address value="<?php if(isset($_SESSION['occurrence']['address'])) echo $_SESSION['occurrence']['address']; ?>" readonly required>
+        <input id=input_address placeholder="Endereço" type=text name=address value="<?php if(isset($_SESSION['occurrence']['address'])) echo $_SESSION['occurrence']['address']; ?>" readonly required>
         <br>
 
-		Latitude: 
-        <input id=input_latitude type=text name=latitude value="<?php if(isset($_SESSION['occurrence']['latitude'])) echo $_SESSION['occurrence']['latitude']; ?>" readonly required>
+        <input id=input_latitude placeholder="Latitude" type=text name=latitude value="<?php if(isset($_SESSION['occurrence']['latitude'])) echo $_SESSION['occurrence']['latitude']; ?>" readonly required>
         <br>
 
-		Longitude: 
-        <input id=input_longitude type=text name=longitude value="<?php if(isset($_SESSION['occurrence']['longitude'])) echo $_SESSION['occurrence']['longitude']; ?>" required readonly>
+        <input id=input_longitude placeholder="Logitude" type=text name=longitude value="<?php if(isset($_SESSION['occurrence']['longitude'])) echo $_SESSION['occurrence']['longitude']; ?>" required readonly>
         <br>
 
-		Dia: 
-        <input id=input_day type=date name=day value="<?php if(isset($_SESSION['occurrence']['day'])) echo $_SESSION['occurrence']['day']; ?>" required>
+        <input id=input_day placeholder="Dia" type=date name=day value="<?php if(isset($_SESSION['occurrence']['day'])) echo $_SESSION['occurrence']['day']; ?>" required>
         <br>
 
-		Hora: 
-        <input id=input_time type=time name=time value="<?php if(isset($_SESSION['occurrence']['time'])) echo $_SESSION['occurrence']['time']; ?>" required>
+        <input id=input_time placeholder="Hora" type=time name=time value="<?php if(isset($_SESSION['occurrence']['time'])) echo $_SESSION['occurrence']['time']; ?>" required>
         <br>
 
         <!-- list markers -->
-        Icone do marcador:
+        <!--Icone do marcador:
         <?php foreach ($markers as $marker) : ?>
             <label for=marker_id<?php echo $marker['id']; ?> >
                 <img src="<?php echo base_url().'assets/images/img_markers/'.$marker['img_name']; ?>" >
@@ -65,7 +67,7 @@
                     required <?php if (isset($_SESSION['occurrence']['marker_id'])) if ($marker['id'] == $_SESSION['occurrence']['marker_id']) echo 'checked' ?>  >
             </label>
         <?php endforeach; ?>
-        <br>
+        <br>-->
 
 		Boletim: 
         <input type=radio name=reported value=1 <?php if(isset($_SESSION['occurrence']['reported'])) if($_SESSION['occurrence']['reported'] == 1) echo 'checked'; ?> required> Sim
@@ -78,13 +80,12 @@
         <br>
 
         <input type=hidden name=complement value='' />
-		Complemento: 
-        <input type=text name=complement value="<?php if(isset($_SESSION['occurrence']['complement'])) echo $_SESSION['occurrence']['complement']; ?>">
+
+        <input type=text placeholder="Complemento" name=complement value="<?php if(isset($_SESSION['occurrence']['complement'])) echo $_SESSION['occurrence']['complement']; ?>">
         <br>
 
         <!-- email for controller -->
-        Email: 
-        <input id=email type=email name=email value="<?php if(isset($_SESSION['occurrence']['email'])) echo $_SESSION['occurrence']['email']; ?>" required>
+        <input id=email placeholder="Email" type=email name=email value="<?php if(isset($_SESSION['occurrence']['email'])) echo $_SESSION['occurrence']['email']; ?>" required>
         <br>
         
         <!-- reCaptcha -->
@@ -92,7 +93,7 @@
 
         <input type=submit value=Enviar>
 	</form>
-
+</div>
 
     <!-- load main js -->
     <?php $this->load->view('includes/main_js'); ?>
